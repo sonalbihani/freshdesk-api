1138b9628e30314aae155b5d27a7183b263da250
Sonal Bihani <Sonal.Bihani@Chubb.com>
2020-07-26 15:37:40 +0530

    resolving imports

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask_cors import CORS
 from flask import Flask, jsonify, request
 from sqlalchemy import text
 from os import environ
-from .entities.entity import Session, engine, Base
-from .entities.models import Tickets, TicketSchema, Contacts, ContactSchema, User, UserSchema
+from .entity import Session, engine, Base
+from .models import Tickets, TicketSchema, Contacts, ContactSchema, User, UserSchema
 from passlib.hash import pbkdf2_sha256
 from datetime import datetime, timedelta
 from flask_jwt_extended import JWTManager,jwt_required, create_access_token
