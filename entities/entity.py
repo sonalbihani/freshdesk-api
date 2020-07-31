ad87de8064cbc28cab053db92e8cec3dbc3c4eb9
Sonal Bihani <Sonal.Bihani@Chubb.com>
2020-07-31 12:13:49 +0530

    changes

--- entities/entity.py
+++ entities/entity.py
@@ -6,11 +6,20 @@ from sqlalchemy.orm import sessionmaker
 db_url = 'sql12.freemysqlhosting.net:3306'
 db_type = 'mysql'
 db_connector = 'pymysql'
-db_name = 'sql12356255'
-db_user = 'sql12356255'
-db_password = 'pqMknJ3YT7'
+db_name = 'sql12357710'
+db_user = 'sql12357710'
+db_password = 'u93NzNHtey'
 db_server ='sql12.freemysqlhosting.net'
 sqlalchemy_dburl = f'{db_type}+{db_connector}://{db_user}:{db_password}@{db_server}/{db_name}'
+
+# db_url = 'sql12.freemysqlhosting.net:3306'
+# db_type = 'mysql'
+# db_connector = 'pymysql'
+# db_name = 'freshdesk'
+# db_user = 'sonalbihani@freshdesk-clone'
+# db_password = 'Lookingforlight1@'
+# db_server ='freshdesk-clone.mysql.database.azure.com:3306'
+# sqlalchemy_dburl = f'{db_type}+{db_connector}://{db_user}:{db_password}@{db_server}/{db_name}'
 engine = create_engine(sqlalchemy_dburl)
 Session = sessionmaker(bind=engine)
 
